8224a10f913315722a2ba31d899c53931d22ecd1
Siddharth <91842997+SiddharthB70@users.noreply.github.com>
2023-02-25 10:02:25 +0530

    Added Static Build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(express.static())
+app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
 
