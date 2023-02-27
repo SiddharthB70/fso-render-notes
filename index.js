d238e63ee0aca54572d2af77d8a13c86a5249fe9
Siddharth <91842997+SiddharthB70@users.noreply.github.com>
2023-02-27 22:30:43 +0530

    Connecting Backend With MongoDB

--- index.js
+++ index.js
@@ -1,55 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Note = require("./models/note");
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
 
-let notes = [
-    {
-        id: 1,
-        content: "hello",
-        important: true,
-    },
-    {
-        id: 2,
-        content: "world",
-        important: false,
-    },
-];
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello World!</h1>");
-});
-
 app.get("/api/notes", (request, response) => {
-    response.json(notes);
+    Note.find({}).then((notes) => {
+        response.json(notes);
+    });
 });
-
-app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find((note) => note.id === id);
-
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter((note) => note.id !== id);
-
-    response.status(204).end();
-});
-
-const generateId = () => {
-    const maxId =
-        notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-    return maxId + 1;
-};
 
 app.post("/api/notes", (request, response) => {
     const body = request.body;
@@ -60,16 +27,32 @@ app.post("/api/notes", (request, response) => {
         });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    };
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then((savedNote) => {
+        response.json(savedNote);
+    });
 });
+
+app.get("/api/notes/:id", (request, response) => {
+    Note.findById(request.params.id)
+        .then((note) => {
+            response.json(note);
+        })
+        .catch((error) => response.status(404).json({ content: "error" }));
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
+
+    response.status(204).end();
+});
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
