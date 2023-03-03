d5640d39520cbee64d222606bbd21dd3002fc0dd
Siddharth <91842997+SiddharthB70@users.noreply.github.com>
2023-03-03 11:53:39 +0530

    Restructured Directory

--- models/note.js
+++ models/note.js
@@ -1,18 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-console.log("Connecting to", url);
-mongoose
-    .connect(url)
-    .then((result) => {
-        console.log("Connected to Database");
-    })
-    .catch((error) => {
-        console.log("Unable to connect to Database");
-    });
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
