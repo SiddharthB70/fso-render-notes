87d22c613ff7658dc233f4180b671a90d945c71d
Siddharth <91842997+SiddharthB70@users.noreply.github.com>
2023-03-19 19:24:36 +0530

    4c

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const notesRouter = require("./controllers/notes");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const userRouter = require("./controllers/users");
 
 mongoose.set("strictQuery", false);
 
@@ -15,10 +16,25 @@ logger.info("connecting to", config.MONGODB_URI);
 mongoose
     .connect(config.MONGODB_URI)
     .then(() => {
-        logger.info("connected to MongoDB");
+        logger.info("Connected to MongoDB (Primary URI)");
     })
     .catch((error) => {
-        logger.error("error connecting to MongoDB:", error.message);
+        logger.error(
+            "Unable to Connect to Primary MongoDB URI. Trying Secondary MongoDB URI",
+            error.message
+        );
+        mongoose
+            .connect(config.SECONDARY_MONGODB_URI)
+            .then(() => {
+                logger.info("Connected to MongoDB (Secondary URI)");
+            })
+            .catch((error) => {
+                logger.error(
+                    "Unable to Connect to Secondary MongoDB URI",
+                    error.message
+                );
+                logger.info(config.SECONDARY_MONGODB_URI);
+            });
     });
 
 app.use(cors());
@@ -27,6 +43,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT;
 const MONGODB_URI =
     process.env.NODE_ENV === "test"
         ? process.env.TEST_MONGODB_URI
         : process.env.MONGODB_URI;
+const SECONDARY_MONGODB_URI =
+    process.env.NODE_ENV === "test"
+        ? process.env.SECONDARY_TEST_MONGODB_URI
+        : process.env.SECONDARY_MONGODB_URI;
+const PORT = process.env.PORT;
 
 module.exports = {
     MONGODB_URI,
     PORT,
+    SECONDARY_MONGODB_URI,
 };
