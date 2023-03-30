96120b5220015f81fa705321a86f344be866533c
Siddharth <91842997+SiddharthB70@users.noreply.github.com>
2023-03-30 22:05:24 +0530

    added testing router

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const config = require("./utils/config");
 const express = require("express");
 require("express-async-errors");
 const app = express();
+const mongoose = require("mongoose");
+
 const cors = require("cors");
-const notesRouter = require("./controllers/notes");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
-const mongoose = require("mongoose");
+
+const notesRouter = require("./controllers/notes");
 const userRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
 
@@ -46,6 +48,11 @@ app.use(middleware.requestLogger);
 app.use("/api/notes", notesRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
+
+if (process.env.NODE_ENV === "test") {
+    const testingRouter = require("./controllers/testing");
+    app.use("/api/testing", testingRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
